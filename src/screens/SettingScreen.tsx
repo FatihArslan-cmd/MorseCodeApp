ed7e3fc9976902d0481adeb4c5df6db1e9875a4d
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-06-26 15:01:02 +0300

    setting screen UI completed

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { View, Text, Switch, ScrollView, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -6,9 +5,6 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 const SettingsScreen = () => {
   const [isBeepEnabled, setIsBeepEnabled] = React.useState(false);
   const [isVibrateEnabled, setIsVibrateEnabled] = React.useState(false);
-  const [isCopyToClipboardEnabled, setIsCopyToClipboardEnabled] = React.useState(false);
-  const [isBatchScanModeEnabled, setIsBatchScanModeEnabled] = React.useState(false);
-  const [isKeepDuplicatesEnabled, setIsKeepDuplicatesEnabled] = React.useState(false);
 
   return (
     <ScrollView style={styles.container}>
@@ -23,35 +19,34 @@ const SettingsScreen = () => {
           <Text style={styles.settingSubText}>Light</Text>
         </View>
         <View style={styles.settingItem}>
-          <Text style={styles.settingText}>Beep</Text>
+          <Icon name="language" size={24} />
+          <Text style={styles.settingText}>Language</Text>
           <Switch value={isBeepEnabled} onValueChange={setIsBeepEnabled} />
         </View>
         <View style={styles.settingItem}>
+          <Icon name="vibration" size={24} />
           <Text style={styles.settingText}>Vibrate</Text>
           <Switch value={isVibrateEnabled} onValueChange={setIsVibrateEnabled} />
         </View>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Help</Text>
         <View style={styles.settingItem}>
-          <Text style={styles.settingText}>Copy to clipboard</Text>
-          <Switch value={isCopyToClipboardEnabled} onValueChange={setIsCopyToClipboardEnabled} />
+          <Icon name="feedback" size={24} />
+          <Text style={styles.settingText}>FeedBack</Text>
         </View>
         <View style={styles.settingItem}>
-          <Text style={styles.settingText}>Batch scan mode</Text>
-          <Text style={styles.settingSubText}>Add option for batch scan to scan screen (Continuous scanning)</Text>
-          <Switch value={isBatchScanModeEnabled} onValueChange={setIsBatchScanModeEnabled} />
+          <Icon name="privacy-tip" size={24} />
+          <Text style={styles.settingText}>Privacy Policy</Text>
         </View>
         <View style={styles.settingItem}>
-          <Text style={styles.settingText}>Camera facing</Text>
-          <Text style={styles.settingSubText}>Back</Text>
+          <Icon name="info" size={24} />
+          <Text style={styles.settingText}>version 1.0.2</Text>
         </View>
-        <View style={styles.settingItem}>
-          <Text style={styles.settingText}>Search engine</Text>
-          <Text style={styles.settingSubText}>Google</Text>
-        </View>
-        <View style={styles.settingItem}>
-          <Text style={styles.settingText}>Keep duplicates</Text>
-          <Text style={styles.settingSubText}>Store duplicate barcodes in history</Text>
-          <Switch value={isKeepDuplicatesEnabled} onValueChange={setIsKeepDuplicatesEnabled} />
-        </View>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.bottomText}>Learn Morse Code</Text>
+        <Text style={styles.copyRightText}>© 2024 Fatih Arslan</Text>
       </View>
     </ScrollView>
   );
@@ -62,6 +57,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f5f5f5',
   },
+  footer: {
+    margin: 20,
+  },
   header: {
     padding: 20,
     backgroundColor: '#ffffff',
@@ -71,6 +69,7 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   section: {
     padding: 20,
@@ -90,6 +89,15 @@ const styles = StyleSheet.create({
   settingText: {
     fontSize: 16,
     flex: 1,
+    marginLeft: 10,
+  },
+  bottomText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  copyRightText: {
+    textAlign: 'center',
   },
   settingSubText: {
     fontSize: 14,
