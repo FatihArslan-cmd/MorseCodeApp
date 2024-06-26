f5d983afff4e7c7baf824d224ae974c89612d683
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-06-26 19:31:14 +0300

    Sound added in chart

--- src/screens/MorseCodeChart.tsx
+++ src/screens/MorseCodeChart.tsx
@@ -1,12 +1,53 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { ScrollView, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { DataTable, Text, Title } from 'react-native-paper';
 import * as Animatable from 'react-native-animatable';
 import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+import { Audio } from 'expo-av';
 import { morseAlphabet } from '../utils/morseAlphabet'; // Importing the Morse alphabet
 
 const MorseCodeChart = () => {
+  const [shortBeep, setShortBeep] = useState(new Audio.Sound());
+  const [longBeep, setLongBeep] = useState(new Audio.Sound());
   const scrollViewRef = useRef(null);
+
+  const loadSounds = async () => {
+    try {
+      await shortBeep.loadAsync(require('../../assets/ShortBeep.mp3'));
+      await longBeep.loadAsync(require('../../assets/LongBeep.mp3'));
+    } catch (error) {
+      console.error('Error loading sound', error);
+    }
+  };
+
+  const unloadSounds = async () => {
+    try {
+      await shortBeep.unloadAsync();
+      await longBeep.unloadAsync();
+    } catch (error) {
+      console.error('Error unloading sound', error);
+    }
+  };
+
+  useEffect(() => {
+    loadSounds();
+    return () => {
+      unloadSounds();
+    };
+  }, []);
+
+  const playBeep = async (code) => {
+    for (let char of code) {
+      if (char === '.') {
+        await shortBeep.replayAsync();
+      } else if (char === '-') {
+        await longBeep.replayAsync();
+      }
+      await new Promise(resolve => setTimeout(resolve, 300));
+    }
+  };
 
   const handleScrollToEnd = () => {
     if (scrollViewRef.current) {
@@ -33,7 +74,10 @@ const MorseCodeChart = () => {
 
             {morseEntries.map(([letter, code]) => (
               <DataTable.Row key={letter}>
-                <DataTable.Cell><Text style={styles.cellText}>{letter}</Text></DataTable.Cell>
+                <DataTable.Cell> <TouchableOpacity style={{flexDirection:'row',}} onPress={() => playBeep(code)}>
+                    <AntDesign name="sound" size={24} color="gray" />
+                 <Text style={styles.cellText}>{letter}</Text>
+                 </TouchableOpacity></DataTable.Cell>
                 <DataTable.Cell><Text style={styles.cellText}>{code}</Text></DataTable.Cell>
               </DataTable.Row>
             ))}
